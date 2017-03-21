2dd5efb06c7661a7ab97ea1b575b20c946c827e2
Undroidd <ghimis.ionut@hotmail.com>
2017-03-22 00:49:00 +0200

    atlceteva

--- src/jenkinstest/JenkinsTest.java
+++ src/jenkinstest/JenkinsTest.java
@@ -17,7 +17,7 @@ public class JenkinsTest {
     public static void main(String[] args) {
         for(int i = 1; i < 10;i ++)
         {
-            System.out.println("elen" + i);
+            System.out.println("elen" + i + 1);
         }
     }
     
